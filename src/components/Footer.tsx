268d6a7cf9e25590177cd659e5011bd66966061e
Elias <e.wambugu192@gmail.com>
2022-12-23 13:10:07 -0500

    added footer link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,19 @@
-import { Flex, Text, Icon } from "@chakra-ui/react";
+import { Flex, Text, Icon, Link } from "@chakra-ui/react";
 import Logo from "./Logo";
 import { Heart } from "react-feather";
+import { Link as RouterLink } from "@chakra-ui/react";
 
 const Footer = () => {
   return (
     <>
-      <Flex mt={10} justify={'center'} w="100%" h={200} bg="background">
+      <Flex mt={10} justify={"center"} w="100%" h={200} bg="background">
         <Logo center={true} />
-        <Flex color='primary' pos='absolute' bottom={5}>
+        <Flex color="primary" pos="absolute" bottom={5}>
           <Text>Made with Love</Text>
-          <Icon ml={2} mr={2} alignSelf='center' as={Heart} color="secondary" />
-          <Text>Elias Wambugu</Text>
+          <Icon ml={2} mr={2} alignSelf="center" as={Heart} color="secondary" />
+          <Link _hover={{color: 'secondary'}} textDecoration={'underline'} to='https://eliaswambugu.com' as={RouterLink}>
+            Elias Wambugu
+          </Link>
         </Flex>
       </Flex>
     </>
